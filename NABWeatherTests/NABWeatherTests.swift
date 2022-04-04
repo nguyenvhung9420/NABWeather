031c49b5b4fb75aaebd44f82f555467e60ffc4e7
Hung Nguyen <hung.nguyen@wearefram.com>
2022-04-04 11:18:27 +0700

    [hungnguyen] [WIP] Adding unit testing

--- NABWeatherTests/NABWeatherTests.swift
+++ NABWeatherTests/NABWeatherTests.swift
@@ -6,9 +6,16 @@
 //
 
 import XCTest
+import RxSwift
+
 @testable import NABWeather
 
 class NABWeatherTests: XCTestCase {
+    
+    private let keywordWhenSuccess: String = "Saigon"
+    private let keywordWhenSuccessLower: String = "saigon"
+    
+    private let keywordWhenFailed: String = "saigo"
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -16,6 +23,52 @@ class NABWeatherTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+     
+    func testSuccessCase() {
+        let disposeBag = DisposeBag()
+        let appServerClient = WeatherService()
+     
+        let viewModel = EntryViewModel()
+        viewModel.getWeatherList(count: 10, city: self.keywordWhenSuccessLower, failure: nil)
+     
+        let expectNormalFriendCellCreated = expectation(description: "Data is fetched with success")
+        viewModel.weatherItems.subscribe(
+            onNext:  { items in
+                var firstCellIsNormal: Bool = false
+                
+                print("items = \(items)")
+                if items.isEmpty == false {
+                    firstCellIsNormal = true
+                }
+                
+                XCTAssertTrue(firstCellIsNormal)
+                expectNormalFriendCellCreated.fulfill()
+                
+            }
+        ).disposed(by: disposeBag)
+     
+        wait(for: [expectNormalFriendCellCreated], timeout:0.1)
+    }
+    
+    func testFailureCase() {
+        let disposeBag = DisposeBag()
+        let appServerClient = WeatherService()
+        
+        let viewModel = EntryViewModel()
+        viewModel.getWeatherList(count: 10, city: self.keywordWhenFailed, failure: nil)
+        
+        let expectNormalFriendCellCreated = expectation(description: "Data is fetched with FAILURE")
+        viewModel.weatherItems.subscribe(
+            onNext:  { items in
+                var firstCellIsNormal: Bool = false
+                firstCellIsNormal = items.isEmpty
+                XCTAssertTrue(firstCellIsNormal)
+                expectNormalFriendCellCreated.fulfill()
+                
+            }
+        ).disposed(by: disposeBag)
+        wait(for: [expectNormalFriendCellCreated], timeout:0.1)
     }
 
     func testExample() throws {
@@ -32,5 +85,6 @@ class NABWeatherTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+
 
 }
